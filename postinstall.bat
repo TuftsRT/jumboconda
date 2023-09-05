78fff8788724d57aedc28487ec4b95382b1c4585
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2023-09-05 17:30:28 -0400

    add remarks to postinstall script

--- postinstall.bat
+++ postinstall.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
 
+REM configure geopandas to use shapely instead of pygeos
+SETX USE_PYGEOS 0 /M
+
+REM configure jupyterlab to disable popups and update checking
 IF NOT EXIST "%PREFIX%\share\jupyter\lab\settings" (
     MKDIR "%PREFIX%\share\jupyter\lab\settings"
 )
-
 (
     ECHO {
     ECHO     "@jupyterlab/apputils-extension:notification": {
@@ -13,36 +16,28 @@ IF NOT EXIST "%PREFIX%\share\jupyter\lab\settings" (
     ECHO }
 ) > "%PREFIX%\share\jupyter\lab\settings\overrides.json"
 
-SETX USE_PYGEOS 0 /M
-
+REM convert start menu entries to camelcase
 IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" (
     REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" "JumboConda"
 )
-
 IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" (
     REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" "JumboConda Prompt.lnk"
 )
-
 IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" (
     REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" "JumboConda PowerShell.lnk"
 )
-
 IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" (
     REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" "JumboConda Bash.lnk"
 )
-
 IF EXIST "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" (
     REN "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" "JumboConda"
 )
-
 IF EXIST "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" (
     REN "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" "JumboConda Prompt.lnk"
 )
-
 IF EXIST "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" (
     REN "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" "JumboConda PowerShell.lnk"
 )
-
 IF EXIST "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" (
     REN "%APPDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" "JumboConda Bash.lnk"
 )
