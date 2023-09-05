edcfb24e7f619ca71dce963327664e83f6649348
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2023-09-05 00:48:31 -0400

    add postinstall script

---
+++ postinstall.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+IF NOT EXIST "%PREFIX%\share\jupyter\lab\settings" (
+    MKDIR "%PREFIX%\share\jupyter\lab\settings"
+)
+
+(
+    ECHO {
+    ECHO     "@jupyterlab/apputils-extension:notification": {
+    ECHO         "checkForUpdates": "false",
+    ECHO         "fetchNews": "false"
+    ECHO     }
+    ECHO }
+) > "%PREFIX%\share\jupyter\lab\settings\overrides.json"
+
+ICACLS "%PREFIX%\Scripts\*.exe" /grant BUILTIN\users:RX /C /Q
+
+IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" (
+    REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Jumboconda" "JumboConda"
+)
+
+IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" (
+    REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Prompt.lnk" "JumboConda Prompt.lnk"
+)
+
+IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" (
+    REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda PowerShell.lnk" "JumboConda PowerShell.lnk"
+)
+
+IF EXIST "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" (
+    REN "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\JumboConda\Jumboconda Bash.lnk" "JumboConda Bash.lnk"
+)
